a4f53ff8375c1986378eb586b1db4ff300c0086a
andersyt <andersmyt@gmail.com>
2022-11-02 17:34:45 +0100

    Started on apiRequest class

--- art.py
+++ art.py
@@ -1,5 +1,17 @@
 import random
 
+class apiRequest:
+    image_request_endpoint = str
+    request_status = True
+    json_response = dict
+    image_link = str
+    
+    #Constructor
+    def __init__(self):
+        self.api_endpoint = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/'
+        self.objectId = createRandomObjectId()
+        self.image_request_endpoint = str(self.api_endpoint) + str(self.objectId)
+        
 
 #Returns a random integer between 1 and 5000            
 def createRandomObjectId():
@@ -7,7 +19,7 @@ def createRandomObjectId():
     return random_objectId
 
 def main():
-    print(createRandomObjectId())
+    requestObject = apiRequest()
 
 if __name__ == "__main__":
     main()
